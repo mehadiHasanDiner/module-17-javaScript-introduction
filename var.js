52a3163f4954c10af7797372b4df4c5ddbe3b249
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-26 03:09:23 +0600

    var.js file created

---
+++ var.js
@@ -0,0 +1,15 @@
+// vary + able = variable
+
+// Numeric
+var money = 45;
+var age = 36;
+var temperature = 50;
+
+// string
+var person = "mehad hasan";
+var location = "ondor killa bandarban";
+
+// Boolean
+// Boolean data type: I can only take the values true or false and written without quotes
+var isFullMarks = true;
+var isSingle = false;
