6f9d90384586117a178f595fc2e485d0d4899f69
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-26 03:30:06 +0600

    variable-namer.js file created

---
+++ variable-name.js
@@ -0,0 +1,31 @@
+// javascript keywords
+// javascript reserve keywords
+
+var price = 29;
+
+// 1. variable name can not be any keywords
+var false = 96;
+var return  = false;
+
+// 2. variable name has to be in on works. no space
+var my home address = "new kahli"
+
+// 3. variable name can not have quotation
+var "name" = "Tom hanks";
+
+// 4. variable name can not starts with a number but can ends with a number 
+var 99club = 112;
+var club25 = 2023;
+
+// 5. how to use long names 
+// can not use dash
+
+var user_name = "bappa raj"
+var usercurrenthomeaddress = "anbor killa bandarban"
+var user_current_home_address = "anbor killa bandarban" // snake case
+var userCurrentHomeAddress = "anbor killa bandarban" // camel case
+var UserCurrentHomeAddress = "anbor killa bandarban" // pascal case
+
+// variable name is case sensitive
+var person = 25;
+var Person = 35;
