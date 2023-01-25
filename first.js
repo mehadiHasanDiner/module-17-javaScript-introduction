9a9a2470c652f58a604a98fdefe8c7d2c1f3e2e9
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-26 02:33:39 +0600

    Math.js file created

--- first.js
+++ first.js
@@ -1,1 +1,4 @@
 console.log(4);
+console.log(
+  "Javascript is a high level, interpreted programming language used to make web pages more interactive. It let's you implement complex and beautiful things/ design on web pages."
+);
---
+++ math.js
@@ -0,0 +1,16 @@
+/****
+ * 1. number => 55, 54, 76.-67
+ * 2. integer, odd, even: 2, 6, 5
+ * 3. float: decimal 2.5, 14.67, 32.69
+ * 4. Operation: add, subtract, multiply, divide, remainder
+ * 5. percent
+ * 6. average
+ * 7. comparison: >, <, !=, >=, <=, >=
+ * 8. oikik niom
+ * 9. 45, 64, 125, 1, 7 : max, min, average, sum
+ * 10. series
+ * :: ExTRA::
+ * 1. prim
+ * 2. permutation
+ * 3. Area
+ * ***/
