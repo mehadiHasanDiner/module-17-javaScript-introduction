d85c84717c7c8bca262b8d8617fdd0064a9a5459
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-26 20:48:36 +0600

    updated

--- conversion.js
+++ conversion.js
@@ -1,5 +1,22 @@
+// var price1 = "31";
+// var price2 = "10";
+
+// var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
 var price1 = "31";
-var price2 = "10";
+var price2 = 10;
 
 var totalPrice = price1 + price2;
 console.log(totalPrice);
+
+var priceNumber = parseInt(price1);
+console.log(priceNumber);
+
+var gpa = "3.41";
+
+// var gpaNumber = parseInt(gpa);
+// console.log(gpaNumber);
+
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
