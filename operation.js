e4a710374482cfac13181f9a5c5e1c5788bdf23e
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-26 18:43:12 +0600

    updated new file

---
+++ operation.js
@@ -0,0 +1,26 @@
+var onionPrice = 20;
+var eggPrice = 10;
+
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+
+// addition, subtraction
+// var totalPrice = onionPrice + eggPrice;
+// var priceDeference = onionPrice - eggPrice;
+
+// console.log(totalPrice);
+// console.log(priceDeference);
+
+// Multiplication
+// var orangePrice = 40;
+// var quantity = 7;
+
+// var totalPrice = orangePrice * quantity;
+// console.log(totalPrice);
+
+// division
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer);
