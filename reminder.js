7a5f8089be36b4f6ae8aba9ccaa82ac3cf71e67c
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-26 22:37:33 +0600

    all updated module finished

---
+++ reminder.js
@@ -0,0 +1,9 @@
+var mango = 11;
+var person = 2;
+
+// / = this means vagfol
+// % = this means vagshes or module operator or reminder
+// console.log(mango % person);
+
+var reminder = mango % person;
+console.log(reminder);
---
+++ typeof.js
@@ -0,0 +1,19 @@
+// var price = 100;
+var price = "100";
+console.log(typeof price);
+
+var isHappy = false;
+console.log(typeof isHappy);
+
+var romantic;
+console.log(typeof romantic);
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+console.log(sum);
+
+sum = sum.toFixed(2);
+console.log(sum);
+
+sum;
